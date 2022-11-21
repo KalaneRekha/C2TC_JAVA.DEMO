0c81a0d00e0dc9aa212e31e1c1a6ddb91e798755
rekha <rekha@REKHA>
2022-11-21 14:35:29 +0530

    This is my first commit

---
+++ src/ADD.java
@@ -0,0 +1,13 @@
+
+public class ADD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=3;
+		int num2 = 2;
+		int add;
+		add = num1+num2;
+		System.out.println(add);
+	}
+
+}
